097aa31ee7ca85b562283c3dbc01acaade852572
Luis <luisdieguezbarrio@gmail.com>
2020-05-13 20:46:01 +0200

    clean code wp header.php

--- WEBSITES/BLOG-WP/code/header.php
+++ WEBSITES/BLOG-WP/code/header.php
@@ -1,29 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>LUDIBA GROUP - OpenSource Hosting</title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-  <meta name="author" content="">
+  <meta name="description" content="Blog de LUDIBA GROUP en donde se pueden leer las últimas novedades">
+  <meta name="keywords" content="ludiba,blog informatica,servicios,organizacion">
+  <meta name="author" content="Luis Dieguez Barrio">
 
-  <!-- Font Awesome if you need it
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-  -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <!-- <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css"> -->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/estilos-min.css">
 
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 
-  <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/gradiente.css">
 
-  <!-- Menú efecto drop -->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/menu-drop.css">
   
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/recuadro.css">
